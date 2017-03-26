114092ae24b7ebf5279cf631f3d7410021fae815
southeast02 <yingfeicaozhang100527@gmail.com>
2017-03-26 18:09:15 +0800

    Synchronized patch: Nova API-Gateway Server Action Support(part1)
    1. What is the problem
    Originally this patch has been committed to the Tricircle and provided
    server action support, but after Tricircle narrowed its scope to
    networking automation across Neutron, these API-Gateway features
    implemented in this patch were no longer used. However Trio2o needs
    these
    functions, so we plan to synchronize this patch from Gerrit to Trio2o.
    You can find the old patch on Gerrit here[1].
    
    The current Nova API-Gateway does not support following server actions:
       os-start: Start server
       os-stop: Stop server
       lock: Lock server
       unlock: Unlock server
       pause: Pause server
       unpause: Unpause server
       resume: Resume a suspended server and change its status to ACTIVE
       suspend: Suspend a server
       shelve: Shelve a server
       unshelve: Unshelve a server
       shelveOffload: Shelf-offload or remove a shelved server
       migrate: Migrate a server to a host. The scheduler chooses the host
       forceDelete: Force-delete a server before deferred cleanup
       trigger_crash_dump: Trigger a crash dump in a server
    
    2. What is the solution to the problem
    Implement the above server actions.
    
    3. What the features need to be implemented to the Tricircle
    to realize the solution
    Add the above server actions.
    
    [1] https://review.openstack.org/#/c/366606/
    
    Change-Id: Ie364735106f742e326b2aef5d961c418d55376c1

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Devstack extras script to install Trio2o
 
 # Test if any trio2o services are enabled
@@ -252,6 +253,7 @@ function move_neutron_server {
     # remove previous failure flag file since we are going to restart service
     rm -f "$SERVICE_DIR/$SCREEN_NAME"/q-svc.failure
     sleep 20
+    enable_service q-svc
     run_process q-svc "$NEUTRON_BIN_DIR/neutron-server --config-file $NEUTRON_CONF --config-file /$Q_PLUGIN_CONF_FILE"
 }
 
