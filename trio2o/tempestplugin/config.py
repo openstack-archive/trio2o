acaf62fd4d1ae127493f5d881882ad8eab96176a
Chandan Kumar <chkumar@redhat.com>
2017-05-21 12:06:05 +0530

    Fixed trio2o tempest plugin config
    The trio2o tempest plugin is using the trio2o project config
    which overrides the tempest config while running tempest plugin tests
    with tempest. It leads to the failure of tempest command. We need to
    use oslo_config for specifiying tempest plugin config for trio2o
    to avoid overriding and avoid the above failure.
    
    Change-Id: Ia8a165d496c6520313f1454d11f98adb8830d027
    Closes-Bug: #1692291

--- trio2o/tempestplugin/config.py
+++ trio2o/tempestplugin/config.py
@@ -13,4 +13,4 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from trio2o.common import config as t_config  # noqa
+from oslo_config import cfg  # noqa
